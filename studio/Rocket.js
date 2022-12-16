f5f90e080939a3123a312eca2bdf731697ea9249
nickimpoco <nickimpoco@gmail.com>
2022-12-15 19:32:10 -0500

    This one's for Tom

---
+++ studio/Rocket.js
@@ -0,0 +1,47 @@
+"use strict";
+exports.__esModule = true;
+exports.Rocket = void 0;
+var Rocket = /** @class */ (function () {
+    function Rocket(name, totalCapacityKg) {
+        this.cargoItems = [];
+        this.astronauts = [];
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
+    Rocket.prototype.sumMass = function (items) {
+        var totalMass = 0;
+        for (var i = 0; i < items.length; i++) {
+            totalMass += items[i].massKg;
+        }
+        return totalMass;
+    };
+    Rocket.prototype.currentMassKg = function () {
+        return this.sumMass(this.cargoItems) + this.sumMass(this.astronauts);
+    };
+    Rocket.prototype.canAdd = function (item) {
+        if (this.currentMassKg() + item.massKg <= this.totalCapacityKg) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    Rocket.prototype.addCargo = function (cargo) {
+        if (this.canAdd(cargo)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    Rocket.prototype.addAstronaut = function (astronaut) {
+        if (this.canAdd(astronaut)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    return Rocket;
+}());
+exports.Rocket = Rocket;
